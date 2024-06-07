d972c87385ddaf3f95c4641b5007c6327c7a8333
i.ermakov <i.ermakov@asol.pro>
2024-06-08 01:11:45 +0300

    critical changes

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 docker compose up -d
 
 docker exec constructor-laravel.test-1 php artisan migrate:fresh --seed
+docker exec constructor-laravel.test-1 composer i
+docker exec constructor-laravel.test-1 npm i
+dcoker exec constructor-laravel.test-1 npm run dev
